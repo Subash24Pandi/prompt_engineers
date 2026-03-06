956357b2d6c4c7897ea2c391dda32a4140be2cca
Subash24Pandi <subashpandiofficial@gmail.com>
2026-03-06 12:43:02 +0530

    Add admin portal and admin account setup

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.get('/', (req, res) => {
     res.redirect('/login');
 });
 
+    // Render the home page with admin and prompt engineer login options
+    res.render('home', { showAdminLogin: true });
+
 app.get('/login', (req, res) => res.render('login'));
 app.get('/register', (req, res) => res.render('register'));
 
